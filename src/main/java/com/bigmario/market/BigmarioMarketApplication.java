cac760c63005ba83db8adae0e992a9c20b744763
Mario Castro <mariocastro.pva@gmail.com>
2023-07-10 10:26:12 -0400

    initial commit

---
+++ src/main/java/com/bigmario/market/BigmarioMarketApplication.java
@@ -0,0 +1,13 @@
+package com.bigmario.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigmarioMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigmarioMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigmario/market/BigmarioMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigmario.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigmarioMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
